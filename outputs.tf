1446d54c8f5a0652cb0170625014b6b0bc7854b0
Nagaraju Golla <118584423+reviewraj@users.noreply.github.com>
2024-09-17 12:16:18 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "The ID of the instance"
-  value       = aws_instance.my_instance.id
-}
-
-output "instance_public_ip" {
-  description = "The public IP address of the instance"
-  value       = aws_instance.my_instance.public_ip
-}
