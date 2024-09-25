f2c5b328b41baf3eacf315179d27f7aa8a73a1f3
Nagaraju Golla <118584423+reviewraj@users.noreply.github.com>
2024-09-25 14:26:27 +0530

    Delete terraform/outputs.tf

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "The ID of the instance"
-  value       = aws_instance.builtbyterraform.id
-}
-
-output "instance_public_ip" {
-  description = "The public IP address of the instance"
-  value       = aws_instance.builtbyterraform.public_ip
-}
