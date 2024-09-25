1a406fe1699536a32445f8203afe8367bef5f2dc
Nagaraju Golla <118584423+reviewraj@users.noreply.github.com>
2024-09-25 16:02:58 +0530

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,14 +1,1 @@
 
-resource "aws_ecr_repository" "medus-backend-server" {
-  name = "medusa-backend-prod"
-  image_tag_mutability = "IMMUTABLE"
-}
-output "ecr_repository_name" {
-  description = "The name of the ECR repository"
-  value       = aws_ecr_repository.medus-backend-server.name
-}
-
-output "ecr_repository_uri" {
-  description = "The URI of the ECR repository"
-  value       = aws_ecr_repository.medus-backend-server.repository_url
-}
